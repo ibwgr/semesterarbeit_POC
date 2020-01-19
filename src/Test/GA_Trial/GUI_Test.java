9ebb09781f0732c28a27f5abdb0cf840b0026a55
tchsaro1 <roberto.salerno@swissscom.com>
2020-01-19 22:25:27 +0100

    [Semsterarbeit] javafx2

--- src/Test/GA_Trial/GUI_Test.java
+++ src/Test/GA_Trial/GUI_Test.java
@@ -1,10 +1,48 @@
 package GA_Trial;
 
-public class GUI_Test  {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import org.junit.Before;
+import org.junit.Test;
+import org.loadui.testfx.GuiTest;
+import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import java.io.IOException;
+import static org.mockito.Mockito.times;
+import static org.loadui.testfx.Assertions.*;
+
+public class GUI_Test extends GuiTest {
 
 
+    @Override
+    protected Parent getRootNode() {
+        Parent parent = null;
 
 
+        try {
+            parent = FXMLLoader.load(getClass().getResource("FXML-View.fxml"));
+            return parent;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+return parent;
     }
 
 
+    @Test
+    public void shouldShowRightText(){
+        Label reiseziel = find("#reiseziel");
+        assertTrue(reiseziel.getText().equals("Nach"));
+    }
+
+    @Test
+    public void shouldClickOnButton(){
+        Button showAll = find("#showAll");
+        click(showAll);
+    }
+}
